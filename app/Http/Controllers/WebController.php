ef583e2342e8f8e378bb0810ee338b85e0a84649
Gafar <qafarzade2014@gmail.com>
2022-02-18 17:43:51 +0400

    front done

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\{Ingredient, Meal};
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function index(Request $request)
+    {
+        $search = $request->get('search');
+
+        return view('welcome')->with([
+            'ingredients' => Ingredient::query()
+                 ->when($search, fn ($query) => $query->where('name', 'like', "%$search%"))->paginate(10),
+            'meals' => Meal::get()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::view('/welcome', 'welcome');
+Route::get('/', [WebController::class, 'index'])->name('homepage');
 
 Route::prefix('admin')->group(function(){
-    \TCG\Voyager\Facades\Voyager::routes();
+Voyager::routes();
 });
 
